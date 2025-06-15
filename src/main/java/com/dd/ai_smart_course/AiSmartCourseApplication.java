72785e630304a2e676e583f1982d7f1c2be05512
nightclassXCR <1246716434@qq.com>
2025-06-15 15:24:48 +0800

    feat:第一次提交

---
+++ src/main/java/com/dd/ai_smart_course/AiSmartCourseApplication.java
@@ -0,0 +1,13 @@
+package com.dd.ai_smart_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSmartCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSmartCourseApplication.class, args);
+    }
+
+}
