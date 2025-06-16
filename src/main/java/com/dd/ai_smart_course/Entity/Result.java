8e246d4908262304820b67ad9dad20a50c3790e3
nightclassXCR <1246716434@qq.com>
2025-06-16 19:10:20 +0800

    feat:设置了一个统一响应封装类

---
+++ src/main/java/com/dd/ai_smart_course/Entity/Result.java
@@ -0,0 +1,60 @@
+package com.dd.ai_smart_course.Entity;
+
+
+
+public class Result<T> {
+    private int code;
+    private String message;
+    private T Data;
+
+    public Result() {}
+    public Result(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        Data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return Data;
+    }
+
+    public void setData(T data) {
+        Data = data;
+    }
+
+    // 成功：默认信息
+    public static <T> Result<T> success(T data) {
+        return new Result<>(200, "操作成功", data);
+    }
+
+    // 成功：自定义信息
+    public static <T> Result<T> success(String message, T data) {
+        return new Result<>(200, message, data);
+    }
+
+    // 失败：默认状态码
+    public static <T> Result<T> error(String message) {
+        return new Result<>(500, message, null);
+    }
+
+    // 失败：自定义状态码和信息
+    public static <T> Result<T> error(int code, String message) {
+        return new Result<>(code, message, null);
+    }
+}
