edc866eead05b3a1a7b5c67271bc0a5bcaaf9372
Arrhenius401 <wu1825270596@126.com>
2025-06-19 22:12:19 +0800

    更新了File,Log和QA类的基础的增删改查功能

--- src/main/java/com/dd/ai_smart_course/service/impl/FileImpl.java
+++ src/main/java/com/dd/ai_smart_course/service/impl/FileImpl.java
@@ -1,12 +1,43 @@
 package com.dd.ai_smart_course.service.impl;
 
+import com.dd.ai_smart_course.entity.File;
 import com.dd.ai_smart_course.mapper.FileMapper;
+import com.dd.ai_smart_course.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class FileImpl {
+public class FileImpl implements FileService {
 
 
+    @Autowired
+    private FileMapper fileMapper;
+
+    @Override
+    public List<File> getAllFiles() {
+        return fileMapper.getAllFiles();
+    }
+
+    @Override
+    public File getFileById(int id) {
+        return fileMapper.getFileById(id);
+    }
+
+    @Override
+    public int addFile(File file) {
+        return fileMapper.addFile(file);
+    }
+
+    @Override
+    public int updateFile(File file) {
+        return fileMapper.updateFile(file);
+    }
+
+    @Override
+    public int deleteFile(int id) {
+        return fileMapper.deleteFile(id);
+    }
 
 }
--- src/main/java/com/dd/ai_smart_course/service/impl/QAImpl.java
+++ src/main/java/com/dd/ai_smart_course/service/impl/QAImpl.java
@@ -1,4 +1,40 @@
 package com.dd.ai_smart_course.service.impl;
 
-public class QAImpl {
+import com.dd.ai_smart_course.entity.QA;
+import com.dd.ai_smart_course.mapper.QAMapper;
+import com.dd.ai_smart_course.service.QAService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QAImpl implements QAService {
+    @Autowired
+    private QAMapper qaMapper;
+
+    @Override
+    public List<QA> getAllQA() {
+        return qaMapper.getAllQAs();
+    }
+
+    @Override
+    public QA getQAById(int id) {
+        return qaMapper.getQAById(id);
+    }
+
+    @Override
+    public int addLog(QA qa) {
+        return qaMapper.addQA(qa);
+    }
+
+    @Override
+    public int updateQA(QA qa) {
+        return qaMapper.updateQA(qa);
+    }
+
+    @Override
+    public int deleteQA(int id) {
+        return qaMapper.deleteQA(id);
+    }
 }
