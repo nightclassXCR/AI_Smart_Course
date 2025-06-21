8b44e7e8606eb9ff27e697dbd79a3b6287eb0a41
lcz2799707969@gmail.com <lcz2799707969@gmail.com>
2025-06-21 21:10:53 +0800

    feat:taskservice加了点事物处理

--- src/main/java/com/dd/ai_smart_course/service/impl/ScoreServiceImpl.java
+++ src/main/java/com/dd/ai_smart_course/service/impl/ScoreServiceImpl.java
@@ -4,13 +4,9 @@ import com.dd.ai_smart_course.entity.Score;
 import com.dd.ai_smart_course.mapper.ScoreMapper;
 import com.dd.ai_smart_course.service.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -18,6 +14,7 @@ public class ScoreServiceImpl implements ScoreService {
 
     @Autowired
     private ScoreMapper scoreMapper;
+
     @Override
     public Score getById(int Id) {
         Score score = scoreMapper.getById(Id);
